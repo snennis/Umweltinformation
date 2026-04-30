dcfcf35b22945e56e747536911417fbc843636bb
snennis <ddennisddv@gmail.com>
2026-04-30 11:19:48 +0200

    change plots

--- code/dom_dgm.R
+++ code/dom_dgm.R
@@ -1,52 +1,222 @@
+############################################################
+# Skript: nDOM und potenzielle Baumhöhen im Bereich der Wiese
+#
+# Beschreibung:
+# Dieses Skript lädt ein Digitales Geländemodell (DGM), ein
+# Digitales Oberflächenmodell (DOM), die Goethepark-Fläche und
+# die Wiesenfläche. DGM und DOM werden auf die Bounding Box der
+# Wiese zugeschnitten. Anschließend wird ein normalisiertes DOM
+# berechnet, um potenzielle Baumhöhen zu filtern.
+#
+# Zusätzlich wird die Wiesen-Geometrie in den Plots dargestellt,
+# damit sichtbar ist, wo die Wiese innerhalb der zugeschnittenen
+# Bounding Box liegt.
+#
+# Eingabedaten:
+# - data/gp_sf.rds
+# - data/wiese_sf.rds
+# - data/dom_dgm/dgm1_33_386_5822_2_be.xyz
+# - data/dom_dgm/dom1_33_386_5822_2_be.txt
+#
+# Ausgabe:
+# - Testplots für DGM, DOM und nDOM
+# - Raster mit potenziellen Baumhöhen
+# - Punktdatensatz aus gefilterten Rasterzellen
+############################################################
+
+
+#### 1) Arbeitsumgebung vorbereiten ####
+
+rm(list = ls())
+
 library(sf)
 library(terra)
 
 
+#### 2) Parameter definieren ####
 
-# Goethepark (Polygon)
-gp_sf <- readRDS("data/gp_sf.rds")
+gp_path <- "data/gp_sf.rds"
+wiese_path <- "data/wiese_sf.rds"
+baeume_path <- "data/baeume_sf.rds"
+
+dgm_path <- "data/dom_dgm/dgm1_33_386_5822_2_be.xyz"
+dom_path <- "data/dom_dgm/dom1_33_386_5822_2_be.txt"
+
+target_crs <- "EPSG:25833"
+
+# Mindesthöhe für potenzielle Bäume in Metern
+min_tree_height <- 1
+
+# Maximalhöhe, um unrealistische Ausreißer auszuschließen
+max_tree_height <- 100
+
+
+#### 3) Eingabedaten prüfen ####
+
+input_files <- c(gp_path, wiese_path, dgm_path, dom_path)
+missing_files <- input_files[!file.exists(input_files)]
+
+if (length(missing_files) > 0) {
+  stop(
+    "Die folgenden Eingabedateien wurden nicht gefunden:\n",
+    paste("-", missing_files, collapse = "\n"),
+    call. = FALSE
+  )
+}
+
+
+#### 4) Goethepark und Wiese laden ####
+
+gp_sf <- readRDS(gp_path)
 gp_sf <- st_transform(gp_sf, 25833)
 
-# auf terra-Format bringen
+wiese_sf <- readRDS(wiese_path)
+wiese_sf <- st_transform(wiese_sf, 25833)
+
+baeume_sf <- readRDS(baeume_path)
+baeume_sf <- st_transform(baeume_sf, 25833)
+
+# sf-Objekte in terra-Vektorobjekte umwandeln
 gp_vect <- vect(gp_sf)
+wiese_vect <- vect(wiese_sf)
+baeume_vect <- vect(baeume_sf)
 
-### 2) DGM & DOM laden (ECHTE DATEN)
-dgm <- rast("data/dgm1_33_386_5822_2_be.xyz")
-dom <- rast("data/dom1_33_386_5822_2_be.txt")
-
-crs(dgm) <- "EPSG:25833"
-crs(dom) <- "EPSG:25833"
+# Bounding Box der Wiese als terra-Extent
+wiese_ext <- ext(wiese_vect)
 
 
-plot(dgm, col = terrain.colors(20), main = "DGM (Gelände)")
-plot(dom, col = terrain.colors(20), main = "DOM (Oberfläche)")
+#### 5) DGM und DOM laden ####
+
+dgm <- rast(dgm_path)
+dom <- rast(dom_path)
+
+crs(dgm) <- target_crs
+crs(dom) <- target_crs
 
 
-# Raster clippen
-dgm_gp <- crop(dgm, gp_vect) |> mask(gp_vect)
-dom_gp <- crop(dom, gp_vect) |> mask(gp_vect)
+#### 6) Testplots für vollständiges DGM und DOM ####
 
-diff_gp <- dom_gp - dgm_gp
+plot(
+  dgm,
+  col = terrain.colors(20),
+  main = "DGM - Digitales Geländemodell"
+)
 
-# alles unter 2.5m entfernen
-diff_gp[diff_gp < 2.5] <- NA
-plot(diff_gp, main = "DOM - DGM (Goethepark)")
+plot(
+  wiese_vect,
+  add = TRUE,
+  border = "red",
+  lwd = 2
+)
 
-# Höhenfilter (z. B. > 2.5 m)
-trees_raster <- diff_gp
-trees_raster[trees_raster < 2.5] <- NA
-plot(trees_raster, main = "Höhenfilter (> 2.5 m)")
+plot(
+  dom,
+  col = terrain.colors(20),
+  main = "DOM - Digitales Oberflächenmodell"
+)
 
-trees_pts <- as.points(diff_gp)
-trees_sf <- st_as_sf(trees_pts)
-nrow(trees_sf)
-# nur "TRUE"-Zellen behalten
-#trees_pts <- trees_pts[trees_pts$lyr.1 == 1, ]
+plot(
+  wiese_vect,
+  add = TRUE,
+  border = "red",
+  lwd = 2
+)
 
-trees_sf <- st_as_sf(trees_pts)
 
-plot(st_geometry(trees_sf), col = "darkgreen", pch = 16, main = "Bäume im Goethepark")
+#### 7) DGM und DOM auf Bounding Box der Wiese zuschneiden ####
 
-global(diff_gp, fun = range, na.rm = TRUE)
+dgm_wiese_bbox <- crop(dgm, wiese_ext)
+dom_wiese_bbox <- crop(dom, wiese_ext)
 
+
+#### 8) Testplots für zugeschnittenes DGM und DOM mit Wiesen-Geometrie ####
+
+plot(
+  dgm_wiese_bbox,
+  col = terrain.colors(20),
+  main = "DGM - zugeschnitten auf BBox der Wiese"
+)
+
+plot(
+  wiese_vect,
+  add = TRUE,
+  border = "red",
+  lwd = 2
+)
+
+
+plot(
+  dom_wiese_bbox,
+  col = terrain.colors(20),
+  main = "DOM - zugeschnitten auf BBox der Wiese"
+)
+
+plot(
+  wiese_vect,
+  add = TRUE,
+  border = "red",
+  lwd = 2
+)
+
+
+#### 9) nDOM berechnen ####
+
+ndom_wiese_bbox <- dom_wiese_bbox - dgm_wiese_bbox
+
+
+#### 10) nDOM plotten mit Wiesen-Geometrie ####
+
+plot(
+  ndom_wiese_bbox,
+  col = terrain.colors(20),
+  main = "nDOM = DOM - DGM, BBox der Wiese"
+)
+
+plot(
+  wiese_vect,
+  add = TRUE,
+  border = "red",
+  lwd = 2
+)
+
+global(ndom_wiese_bbox, fun = range, na.rm = TRUE)
+
+
+#### 11) Potenzielle Baumhöhen filtern ####
+
+trees_raster <- ndom_wiese_bbox
+
+trees_raster[trees_raster < min_tree_height] <- NA
+trees_raster[trees_raster > max_tree_height] <- NA
+
+
+#### 12) Potenzielle Baumhöhen plotten mit Wiesen-Geometrie ####
+
+plot(
+  trees_raster,
+  col = terrain.colors(20),
+  main = paste0(
+    "Potenzielle Baumhöhen in der Wiesen-BBox (",
+    min_tree_height,
+    " bis ",
+    max_tree_height,
+    " m)"
+  )
+)
+
+plot(
+  wiese_vect,
+  add = TRUE,
+  border = "red",
+  lwd = 2
+)
+
+plot(
+  baeume_vect,
+  add = TRUE,
+  border = "black",
+  lwd = 2
+)
+
+global(trees_raster, fun = range, na.rm = TRUE)
 
